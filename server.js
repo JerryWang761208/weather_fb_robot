c68f066c1c6e31e066fc16c173c7d94f90fa1664
Jerry <c1495616@gmail.com>
2016-12-27 21:06:02 +0800

    modification

--- server.js
+++ server.js
@@ -2,26 +2,35 @@ var login = require('facebook-chat-api');
 var getWeather = require('./getWeather');
 var port = process.env.PORT || 8080;
 var express = require('express');
-
+var app = express();
 var account = {
     email: 'c1495616@gmail.com',
     password: 'jerry210492'
 };
 
-login(account, function(err, api){
+
+
+
+app.get('/', function(req, res) {
+    login(account, function(err, api){
     /*
     {
         my:733447275,
         flower:100000146521584
     }
     */ 
-    var id = 733447275;
-    var interval = 3   * 1000; // 3 hour
+        var id = 733447275;
+        var interval = 3   * 1000; // 3 hour
 
-    setInterval(function(){
-        getWeather(function(err, weather){
-            api.sendMessage(weather, id);
-        });
-    }, interval);
+        setInterval(function(){
+            getWeather(function(err, weather){
+                api.sendMessage(weather, id);
+            });
+        }, interval);
     
+    });
+});
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
 });
