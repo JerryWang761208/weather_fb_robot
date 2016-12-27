54e2f3c05ead4e166a63d5e6b742dc4e52d81c13
Jerry <c1495616@gmail.com>
2016-12-27 21:40:40 +0800

    mod

---
+++ people.js
@@ -0,0 +1,16 @@
+var people = [
+    {
+        id:"100000213023243",
+        name:"wan_ting"
+    },
+    {
+        id:"508923564",
+        name:"badow"
+    },
+    {
+        id:"733447275",
+        name:"me"
+    },
+];
+
+module.exports = people;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var getWeather = require('./getWeather');
 var port = process.env.PORT || 8080;
 var express = require('express');
 var app = express();
+var people = require('./people');
 var account = {
     email: 'c1495616@gmail.com',
     password: 'jerry210492'
@@ -19,12 +20,17 @@ app.get('/', function(req, res) {
         flower:100000146521584
     }
     */ 
-        var id = 733447275;
-        var interval = 3   * 1000; // 3 hour
+        // var id = 733447275;
+        var interval = 12 * 3600 * 1000; // 12 hour
+
+
 
         setInterval(function(){
             getWeather(function(err, weather){
-                api.sendMessage(weather, id);
+                people.map(function(e){
+                    api.sendMessage(weather, e.id);
+                });
+                
             });
         }, interval);
     
